f722da2d7b00ec8c8c973335e95593f53d6e3097
Mihkel Eidast <mihkel@play.ee>
2017-07-07 20:45:32 +0300

    [GRZ-58] settings: add settings

---
+++ assets/js/components/settings.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const $          = global.jQuery;
+const storage    = require('../storage');
+
+class Settings {
+
+    constructor(el){
+
+        const self = this;
+
+        this._el                = $(el);
+        this._simpleToggleState = storage.get(`settings.simpleToggle`, false);
+        this._simpleToggle      = this._el.find('[data-role="simple-ui"]');
+        this._simpleClass       = 'has-simple-ui';
+
+        this._init();
+    }
+
+    _init() {
+        this._simpleToggle.on('change', (event) => {
+            const _self = $(event.currentTarget);
+
+            if (_self.is(':checked') ) {
+                $('body').addClass(this._simpleClass);
+                storage.set(`settings.simpleToggle`, true);
+            } else {
+                $('body').removeClass(this._simpleClass);
+                storage.set(`settings.simpleToggle`, false);
+            }
+        });
+
+        if (this._simpleToggleState) {
+            this._simpleToggle.prop('checked', true).trigger('change');
+        }
+    }
+}
+
+module.exports = Settings;
