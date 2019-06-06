78cec2032e2b8fede444871a225b835696744170
Mihkel Eidast <mihkel@play.ee>
2019-06-06 20:30:42 +0300

    add icons test toggle to settings

--- assets/js/components/settings.js
+++ assets/js/components/settings.js
@@ -13,6 +13,10 @@ class Settings {
         this._simpleToggle      = this._el.find('[data-role="simple-ui"]');
         this._simpleClass       = 'has-simple-ui';
 
+        this._iconTestToggleState = storage.get(`settings.iconTest`, false);
+        this._iconTestToggle      = this._el.find('[data-role="test-icons"]');
+        this._iconTestClass       = 'is-icon-test-active';
+
         this._init();
     }
 
@@ -31,6 +35,22 @@ class Settings {
 
         if (this._simpleToggleState) {
             this._simpleToggle.prop('checked', true).trigger('change');
+        }
+
+        this._iconTestToggle.on('change', (event) => {
+            const _self = $(event.currentTarget);
+
+            if (_self.is(':checked') ) {
+                $('body').addClass(this._iconTestClass);
+                storage.set(`settings.iconTest`, true);
+            } else {
+                $('body').removeClass(this._iconTestClass);
+                storage.set(`settings.iconTest`, false);
+            }
+        });
+
+        if (this._iconTestToggleState) {
+            this._iconTestToggle.prop('checked', true).trigger('change');
         }
     }
 }
