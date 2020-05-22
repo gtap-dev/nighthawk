1a5aac02a527385a6b4727c55b35f6ed6dd0e740
Mihkel Eidast <mihkel@play.ee>
2020-05-22 19:37:48 +0300

    setup stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['stylelint-config-standard'],
+    plugins: ['stylelint-scss'],
+    rules: {
+        indentation: 4,
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true,
+        'no-descending-specificity': null,
+        'font-family-no-missing-generic-family-keyword': null,
+    },
+};
